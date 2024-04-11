fd5a671caab7842e1cf7ee77464ef88173248f30
Gorostiola <goroztiola@gmail.com>
2024-04-11 12:55:13 -0600

    Carga de codigo modificado
    11/04/2024
    
    Ejecutor: Jesus Goros.
    
    Descripcion: 1ra carga de codigo

---
+++ BussUsuario/BUsuario.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using DataUsuario;
+using EntityUsuario;
+
+namespace BussUsuario
+{
+    public class BUsuario
+    {
+        DUsuario data = new DUsuario();
+
+        public List<EUsuario> Obtener()
+        {
+            DataTable dt = data.Obtener();
+            List<EUsuario> ls = new List<EUsuario>();
+            foreach (DataRow dr in dt.Rows)
+            {
+                EUsuario us = new EUsuario();
+                us.id = Convert.ToInt32(dr["Id"]);
+                us.nombre = Convert.ToString(dr["Nombre"]);
+                us.apellidop = Convert.ToString(dr["ApellidoP"]);
+                us.apellidom = Convert.ToString(dr["ApellidoM"]);
+                us.fecha = Convert.ToDateTime(dr["FechaNaci"]).ToString("dd-MM-yyyy");
+                us.edad = Convert.ToInt32(dr["Edad"]);
+                ls.Add(us);
+            }
+            return ls;
+        }
+
+        public List<EUsuario> Buscar(string valor)
+        {
+            DataTable dt = data.Buscar(valor);
+            List<EUsuario> ls = new List<EUsuario>();
+            foreach(DataRow dr in dt.Rows)
+            {
+                EUsuario us = new EUsuario();
+
+                us.id = Convert.ToInt32(dr["Id"]);
+                us.nombre = Convert.ToString(dr["Nombre"]);
+                us.apellidop = Convert.ToString(dr["ApellidoP"]);
+                us.apellidom = Convert.ToString(dr["ApellidoM"]);
+                us.fecha = Convert.ToDateTime(dr["FechaNaci"]).ToString("dd-MM-yyyy");
+                us.edad = Convert.ToInt32(dr["Edad"]);
+                ls.Add(us);
+            }
+            return ls;
+        }
+
+
+        public EUsuario ObtenerId(int id)
+        {
+            DataRow dr = data.ObtenerId(id);
+            EUsuario us = new EUsuario();
+
+            us.id = Convert.ToInt32(dr["Id"]);
+            us.nombre = Convert.ToString(dr["Nombre"]);
+            us.apellidop = Convert.ToString(dr["ApellidoP"]);
+            us.apellidom = Convert.ToString(dr["ApellidoM"]);
+            us.fecha = Convert.ToDateTime(dr["FechaNaci"]).ToString("yyyy-MM-dd");
+            us.edad = Convert.ToInt32(dr["Edad"]);
+            return us;
+        }
+
+        public void Agregar(EUsuario us)
+        {
+            int res = data.Agregar(us.nombre, us.apellidop, us.apellidom, us.fecha);
+            if (res != 1)
+                throw new ApplicationException("Error al agregar");
+        }
+
+        public void Editar(EUsuario us)
+        {
+            int res = data.Editar(us.nombre, us.apellidop, us.apellidom, us.fecha, us.id);
+            if (res != 1)
+                throw new ApplicationException("Error al editar");
+        }
+
+        public void Eliminar(EUsuario us)
+        {
+            int res = data.Eliminar(us.id);
+            if (res != 1)
+                throw new ApplicationException("Error al eliminar");
+        }
+    }
+}
---
+++ PracticandoPersonas/Controllers/HomeController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls.WebParts;
+using BussUsuario;
+using EntityUsuario;
+using System.Data;
+using System.Web.Management;
+using System.Data.SqlClient;
+
+namespace PracticandoPersonas.Controllers
+{
+    
+    public class HomeController : Controller
+    {
+        BUsuario buss = new BUsuario();
+        
+        public ActionResult Index()
+        {
+            try
+            {
+                List<EUsuario> ls = buss.Obtener();
+                return View(ls);
+            }
+            catch (Exception ex)
+            {
+                TempData["error"] = ex.Message;
+                return View(new List<EUsuario>());
+            }
+        }
+
+
+        public ActionResult Buscar(string valor)
+        {
+            try
+            {
+                List<EUsuario> ls = buss.Buscar(valor);
+                return View("Index",ls);
+            }
+            catch (Exception ex)
+            {
+                TempData["error"] = ex.Message;
+                return View("Index",new List<EUsuario>());
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Agregar()
+        {
+            return View();
+        }
+
+        public ActionResult Agregar(EUsuario us)
+        {
+            try
+            {
+                buss.Agregar(us);
+                TempData["mensaje"] = "Se Agrego Correctamente";
+                return RedirectToAction("Index");
+
+            }
+            catch (Exception ex)
+            {
+                TempData["error"] = ex.Message;
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Editar(int id)
+        {
+            try
+            {
+                EUsuario us = buss.ObtenerId(id);
+                return View(us);
+            }
+            catch (Exception ex)
+            {
+                TempData["error"] = ex.Message;
+                throw;
+            }
+        }
+
+        public ActionResult Editar(EUsuario us)
+        {
+            try
+            {
+                buss.Editar(us);
+                TempData["mensaje"] = "Se edito correctamente";
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                TempData["error"] = ex.Message;
+                return View();
+            }
+        }
+
+        public ActionResult Eliminar(int id)
+        {
+            try
+            {
+                EUsuario us = buss.ObtenerId(id);
+                buss.Eliminar(us);
+                return RedirectToAction("Index");
+
+            }
+            catch (Exception ex)
+            {
+                TempData["error"] = ex.Message;
+                throw;
+            }
+        }
+
+
+
+
+
+    }
+
+}
